2e432588f6e458cf680545cafb4a7413d3bae11b
Azure <darknightmarehunter@gmail.com>
2021-03-06 02:00:32 -0500

    Fix stupid.

--- Iroh-Library/Iroh/Beacon.cpp
+++
@@ -1,6 +0,0 @@
-#include "Beacon.h"
-
-namespace Iroh {
-	Beacon::Beacon() : controlServer(DEFAULT_CONTROLSERVER), port(DEFAULT_PORTNUMBER), sleepTime(DEFAULT_SLEEPTIMER)
-	{ /* No functionality here */ }
-}
